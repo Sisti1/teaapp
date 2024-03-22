4ad537622e30bc116fdef7f38c46dfb47bd2fe03
keshavpar <keshavparihar54@gmail.com>
2024-03-22 23:06:36 +0530

    Updated The carousel and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,92 @@
 // pages/index.js
-import React from 'react';
+import React, { useRef } from 'react';
 import "./index.css";
-import { Carousel } from "react-bootstrap";
+import image1 from  "../pages/images/img1.jpg";
+import image2 from "../pages/images/img2.jpg";
+import image3  from "../pages/images/img3.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css"
 
 const Home = () => {
-return (
-	<div className='div1'
-	style={{
-		
-		height: '530px',
-		backgroundColor:'#F8DC88',
-}}
-	>
-<div className='div2'>
-	
-<Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://png.pngtree.com/thumb_back/fw800/background/20231103/pngtree-elegant-coffee-cups-against-a-backdrop-of-black-textured-wallpaper-image_13763663.png"
-          alt="First slide"
-        />
-		<Carousel.Caption>
-          <h3>First Slide</h3>
-          <p>This is some sample text for the first slide.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://png.pngtree.com/thumb_back/fw800/background/20231103/pngtree-elegant-coffee-cups-against-a-backdrop-of-black-textured-wallpaper-image_13763663.png"
-          alt="Second slide"
-        />
-		<Carousel.Caption>
-          <h3>First Slide</h3>
-          <p>This is some sample text for the first slide.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://png.pngtree.com/thumb_back/fw800/background/20231103/pngtree-elegant-coffee-cups-against-a-backdrop-of-black-textured-wallpaper-image_13763663.png"
-          alt="Third slide"
-        />
-		<Carousel.Caption>
-          <h3>First Slide</h3>
-          <p>This is some sample text for the first slide.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-		</div>
-	</div>
-);
+  return (
+    <div
+      className='div1'
+      style={{
+        height: '30%',
+        backgroundColor: '#F8DC88',
+      }}
+    >
+      <div className='div2'>
+        <AutoPlayMethods />
+      </div>
+    </div>
+  );
+};
+
+const AutoPlayMethods = () => {
+  let sliderRef = useRef(null);
+  // const play = () => {
+  //   sliderRef.slickPlay();
+  // };
+  // const pause = () => {
+  //   sliderRef.slickPause();
+  // };
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="carousel-container">     
+      <Slider ref={(slider) => (sliderRef = slider)} {...settings}>
+        <div>
+        <img src={image1} alt="" className="slider-image" />
+
+        </div>
+        <div>
+        <img src={image2} alt="" className="slider-image" />
+
+        </div>
+        <div>
+        <img src={image3} alt="" className="slider-image" />
+
+        </div>       
+      </Slider>      
+    </div>
+  );
 };
 
 export default Home;
