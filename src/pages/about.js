e6ce8a128e19a5d694c29383cd6b82baf9a2e9ed
Sisti1 <shristigoyal516@gmail.com>
2024-03-26 12:52:36 +0530

    about page creation

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,28 @@
-
-
 import React from "react";
-import OvalContainer from './OvalContainer';
+import './about.css';
+import { Link } from "react-router-dom";
 const About = () => {
 	return (
-		<div></div>
+		<div className="about-us-container">
+      <div className="about-us-image">
+        <img src="https://www.munatycooking.com/wp-content/uploads/2014/04/Indian-Tea-Karak-Chai-feature-image-2022-500x500.jpg" alt="About Us" />
+      </div>
+      <div className="about-us-content">
+        <h2>About Our Tea Application</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget
+          turpis ut risus semper varius. Vestibulum ac enim vitae nulla
+          eleifend sollicitudin vel in ipsum. Integer id dolor sit amet nibh
+          imperdiet luctus.
+        </p>
+        <p>
+          Sed ac metus eget lacus pretium aliquet. Integer ut augue et velit
+          rutrum aliquam. Fusce nec tortor nec nisl volutpat pretium. Nam nec
+          mi a sapien mollis posuere.
+        </p>
+        <Link to="/teashop" className="explore-button">Explore More</Link>
+      </div>
+    </div>
 	  );
 };
 
