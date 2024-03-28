9209ebf328b8b0cb56467bc2b2d89f22c17089db
keshavpar <keshavparihar54@gmail.com>
2024-03-28 22:00:56 +0530

    Email Functionality Working

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,94 +3,98 @@ import emailjs from 'emailjs-com'; // Import the EmailJS library
 import "./contact.css";
 
 function Contact() {
-    const [formData, setFormData] = useState({
-      name: '',
-      email: '',
-      feedback: ''
-    });
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData(prevState => ({
-        ...prevState,
-        [name]: value
-      }));
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    feedback: ''
+  });
 
-      // Send email using EmailJS
-      emailjs.sendForm('service_qd1zee6', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-        .then((result) => {
-          console.log(result.text);
-          alert('Feedback sent successfully!');
-          setFormData({
-            name: '',
-            email: '',
-            feedback: ''
-          });
-        }, (error) => {
-          console.log(error.text);
-          alert('Failed to send feedback. Please try again later.');
+  const handleChange = (e) => {
+    console.log(e.target)
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cf78lxo', 'template_f0ofvu2', e.target, '5-_n7CJgHvgZtjw48')
+      .then((response) => {
+
+        alert('Feedback sent successfully!');
+        setFormData({
+          name: '',
+          email: '',
+          feedback: ''
         });
-    };
-  
-    return (
-        <div className="contact-container">
-            <div className="contact-details">
-                
-                <div>
-                    <h2>Get in touch</h2>
-                    <p>We are a company with trustworthy customers since 23 years.</p>
-                    <p>With each sip, we embark on a journey of tranquility and enlightenment, embracing the harmony of nature and the wisdom of centuries past.</p>
-                </div>
-                <div>
-                    <h3>Our Contact Information</h3>
-                    <p>Email: globaltradingcompany@gmail.com</p>
-                    <p>Phone: 9719562218</p>
-                    <h3>Opening Hours</h3>
-                    <p>Tuesday/sunday 9:30-7:30</p>
-                    <p>Monday Closed</p>
-                </div>
-            </div>
-            <div className="feedback-form">
-                <h2>Send Us Your Feedback</h2>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="name">Name:</label><br />
-                    <input 
-                        type="text" 
-                        id="name" 
-                        name="name" 
-                        value={formData.name} 
-                        onChange={handleChange} 
-                        required 
-                    /> <br />
-    
-                    <label htmlFor="email">Email Address:</label><br />
-                    <input 
-                        type="email" 
-                        id="email" 
-                        name="email" 
-                        value={formData.email} 
-                        onChange={handleChange} 
-                        required 
-                    />  <br />
-    
-                    <label htmlFor="feedback">Feedback:</label><br />
-                    <textarea 
-                        id="feedback" 
-                        name="feedback" 
-                        value={formData.feedback} 
-                        onChange={handleChange} 
-                        required 
-                    ></textarea>  <br />
-    
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+      }, (err) => {
+        alert('Failed to send feedback. Please try again later.');
+        console.log('FAILED...', err);
+      });
+
+
+  };
+
+  return (
+
+    <div className="contact-container">
+
+      <div className="contact-details">
+
+        <div>
+          <h2>Get in touch</h2>
+          <p>We are a company with trustworthy customers since 23 years.</p>
+          <p>With each sip, we embark on a journey of tranquility and enlightenment, embracing the harmony of nature and the wisdom of centuries past.</p>
         </div>
-    );
+        <div>
+          <h3>Our Contact Information</h3>
+          <p>Email: globaltradingcompany@gmail.com</p>
+          <p>Phone: 9719562218</p>
+          <h3>Opening Hours</h3>
+          <p>Tuesday/sunday 9:30-7:30</p>
+          <p>Monday Closed</p>
+        </div>
+      </div>
+      <div className="feedback-form">
+        <h2>Send Us Your Feedback</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label><br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          /> <br />
+
+          <label htmlFor="email">Email Address:</label><br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />  <br />
+
+          <label htmlFor="feedback">Feedback:</label><br />
+          <textarea
+            id="feedback"
+            name="feedback"
+            value={formData.feedback}
+            onChange={handleChange}
+            required
+          ></textarea>  <br />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
